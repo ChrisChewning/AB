3304be69fa2a6a485cb5d2c905157f7d8b552243
Chris <chewning.cl@gmail.com>
2019-05-29 22:31:44 -0500

    firebase connected w posts 1

--- src/Reducers/PostReducer.js
+++ src/Reducers/PostReducer.js
@@ -4,8 +4,12 @@ import {FETCH_POSTS} from '../Actions/PostActions';
 export default function (state ={}, action) {
   switch (action.type) {
     case FETCH_POSTS:
-    return action.payload; //all the posts we have. any time we get a post we'll dispatch an action to update our props to say 'heres all our new posts.'
+    return action.payload;
     default:
       return state;
   }
 }
+
+
+//you import the action {FETCH_POSTS} to the reducer here.
+//all the posts we have. any time we get a post we'll dispatch an action to update our props to say 'heres all our new posts.'
